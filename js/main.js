76c971dc33a3fd40ae33f7992b10aeea5cf76526
Mirehiko <mirehiko@localhost.localdomain>
2018-02-17 19:41:11 +0500

    Смена картинок при наведении

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var img_timer;
+
 var img = $('.item_box .img-item'); 
 img.on('mouseover', function(e) {
 	var elem = $(e.target);
@@ -5,13 +7,24 @@ img.on('mouseover', function(e) {
 	var length = +$(e.target).attr('data-size');
 	var step = 1;
 	var path = $(e.target).attr('data-original');
-	path = path.substr(0, path.length-5);
 	var format = path.substr(-4);
+	path = path.substr(0, path.length-5);
 
-	step++;
-	e.target.src = path + step + format;
+	// step++;
+
+	// $(e.target).attr('src', path + step + format);
 	
-	console.log(e.target.src);
+	img_timer = setTimeout(function tick() {
+		step++;
+		if (step > length) {
+			step = 1;
+		}
+		console.log(step);
+		$(e.target).attr('src', path + step + format);
+		img_timer = setTimeout(tick, 1000);
+	}, 1000);
+
+	// console.log(e.target.src);
 	// setTimeout(function(){
 	// 	if (step < length) {
 	// 		step++;
@@ -21,5 +34,6 @@ img.on('mouseover', function(e) {
 	// },1000);
 });
 img.on('mouseleave', function(e) {
+	clearTimeout(img_timer);
 	e.target.src = $(e.target).attr('data-original');
 });
